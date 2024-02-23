93648922165de0152d503873235284ef220a5315
Inapogui Michel zeze <153550531+Michel-2a@users.noreply.github.com>
2024-02-23 22:14:43 +0100

    Create home.js

---
+++ home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./assets/css/index.css";
+import Cards from './components/Cards';
+
+const Index = () => {
+    return (
+        <div className="card">
+       
+       <Cards />
+       <Cards />
+       <Cards />
+       <Cards />
+       <Cards />
+       <Cards />
+       <Cards />
+       <Cards />
+
+        
+         </div>
+
+         
+    );
+};
+
+export default Index;
